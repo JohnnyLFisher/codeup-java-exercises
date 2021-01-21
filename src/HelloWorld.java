21692aabfff8fd97321e739887d60cf3ec0c9b5a
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-01-21 14:32:04 -0600

    Java Syntax Exercise Finish

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,86 +1,97 @@
 public class HelloWorld {
 	public static void main(String[] args) {
-		/* Introduction to Java Exercise */
+
+		/** Introduction to Java Exercise **/
+
+
 		//	TODO: "Hello, World!" using System.out.println
-		System.out.println("Hello, World!");
+
+//		System.out.println("Hello, World!");
+
 
 		//	TODO: "Hello, World!" using System.out.print
-		System.out.print("Hello");
-		System.out.print(", World!");
 
-		/* Syntax, Types, and Variables Exercise */
+//		System.out.print("Hello");
+//		System.out.print(", World!");
+
+
+		/** Syntax, Types, and Variables Exercise **/
+
+
 		// TODO: Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
 
+//		int myFavoriteNumber = 5;
+
+//		System.out.println(myFavoriteNumber);
+
+
 		// TODO: Create a String variable named myString and assign a string value to it, then print the variable out to the console.
-
 		// TODO: Change your code to assign a character value to myString.
-		//  What do you notice?
-
 		// TODO: Change your code to assign the value 3.14159 to myString.
-		//  What happens?
+
+//		String myString = "Five";
+//		String myString = 'F';
+//		String myString = 3.14159;
+
+//		System.out.println(myString);
+
 
 		// TODO: Declare an long variable named myNumber, but do not assign anything to it.
 		//  Next try to print out myNumber to the console.
-		//  What happens?
-
 		// TODO: Change your code to assign the value 3.14 to myNumber.
-		//  What do you notice?
-
 		// TODO: Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
-
 		// TODO: Change your code to assign the value 123 to myNumber.
-		//  Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
-
 		// TODO: Change your code to declare myNumber as a float.
 		//  Assign the value 3.14 to it. What happens?
-		//  What are two ways we could fix this?
 
-		// TODO: Copy and paste the following code blocks one at a time and execute them.
-		//  --
-		//  int x = 5;
-		//  System.out.println(x++);
-		//  System.out.println(x);
-		//  --
-		//  int x = 5;
-		//  System.out.println(++x);
-		//  System.out.println(x);
-		//  --
-		//  What is the difference between the above code blocks? Explain why the code outputs what it does.
+//		long myNumber;
+//		myNumber = 3.14;
+//		myNumber = 123L;
+//		myNumber = 123;
+
+//		System.out.println(myNumber);
+
+
+		// TODO: Uncomment the following code blocks one at a time and execute them.
+
+//		  int x = 5;
+//		  System.out.println(x++);
+//		  System.out.println(x);
+
+//		  int x = 5;
+//		  System.out.println(++x);
+//		  System.out.println(x);
+
 
 		// TODO: Try to create a variable named class.
-		//  What happens?
+
+//		Object class;
 
 		// TODO: Object is the most generic type in Java. You can assign any value to a variable of type Object.
 		//  What do you think will happen when the following code is run?
-		//  --
-		//  String theNumberThree = "three";
-		//  Object o = theNumberThree;
-		//  int three = (int) o;
-		//  --
-		//  Copy and paste the code above and then run it.
-		//  Does the result match with your expectation?
-		//  How is the above example different from the code block below?
-		//  --
-		//  int three = (int) "three";
-		//  --
-		//  What are the two different types of errors we are observing?
+		//  Uncomment the code then run it.
+
+//		  String theNumberThree = "three";
+//		  Object o = theNumberThree;
+//		  int three = (int) o;
+
+//		  int three = (int) "three";
+
 
 		// TODO: Rewrite the following expressions using the relevant shorthand assignment operators:
-		//  --
-		//  int x = 4;
-		//  x = x + 5;
-		//  int x = 3;
-		//  int y = 4;
-		//  y = y * x;
-		//  int x = 10;
-		//  int y = 2;
-		//  x = x / y;
-		//  y = y - x;
-		//  --
 
-		// TODO: What happens if you assign a value to a numerical variable that is larger (or smaller) than the type can hold?
-		//  What happens if you increment a numeric variable past the type's capacity?
-		//Hint: Integer.MAX_VALUE is a class constant (we'll learn more about these later) that holds the maximum value for the int type.
+//		  int x = 4;
+//		  x += 5;
+//		  int x = 3;
+//		  int y = 4;
+//		  y *= x;
+//		  int x = 10;
+//		  int y = 2;
+//		  x /= y;
+//		  y -= x;
+
+//		System.out.println(x);
+//		System.out.println(y);
 
 	}
 }
