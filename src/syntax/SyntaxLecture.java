0a4abdf11114fdff86c00758d234534b664c6422
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-01-21 09:21:32 -0600

    Java Syntax Lecture Start

---
+++ src/syntax/SyntaxLecture.java
@@ -0,0 +1,115 @@
+package syntax;
+
+public class SyntaxLecture {
+
+	public static void main(String[] args) {
+
+		// ================== STATEMENTS (s-3)
+
+		// Statements may be...
+		// Declarations
+
+		// Assignment expressions
+
+		// Any use of ++ or --
+
+		// Method invocations
+
+		// Object creation expressions
+
+		// ================== COMMENTS (s-4)
+
+
+
+//        see JavaDocTest in docs package
+
+
+		// ================== DATA TYPES (s-5, 6, 7)
+
+
+		// use the smallest type needed for the job
+
+		// primitive types...
+
+//            byte age = -32;
+//        System.out.println(age);
+//            short minimumAnnualSalary = 15080;
+//            int distanceToSaturn = 1200000000;
+//            long numberOfHumanCells = 3000000000L; // L needed for compiler
+//            System.out.println(numberOfHumanCells);
+//            float lengthOfBacteriaInInches = 0.00004F; // F needed for compiler
+//            System.out.println(lengthOfBacteriaInInches);
+//            double widthOfAtomInMeters = .00000000001;
+//            char seatingSection = 'M'; // must wrap in single quotes
+//            boolean everythingIsAwesome = false;
+
+		// strings (not a primitive type)
+
+//                 String thisIsAString = "Hello";
+
+
+//              like in JS, strings must escape certain characters like quotes and use it to inject newline characters and other formatting
+
+
+
+		// ================== VARIABLES (s-8)
+
+		// declaration separate from initialization
+
+
+
+		// declaration and initialization
+
+
+
+		// ================== IDENTIFIERS AND KEYWORDS (s-9)
+
+		// see curriculum
+
+
+		// ================== CONSTANTS (s-10)
+
+		// see curriculum
+
+
+		// ================== EXPRESSIONS (s-11)
+
+		// basically the same as JS
+
+
+		// ================== OPERATORS (s-12)
+
+		// basically the same as JS
+
+
+		// ================== ASSIGNMENT STATEMENTS (s-13)
+
+		// basically the same as JS
+
+
+		// ================== CASTING (s-14)
+
+		// Link on casting... http://www.javainterviewpoint.com/type-casting-java-implicit-explicit-casting/
+
+
+		// IMPLICIT
+
+
+		// EXPLICIT
+
+		// implicit casting - less to more precise (widening)
+
+//                 float num1 = 2.22F;
+//                 double num2 = num1;
+
+
+		// explicit casting - more to less explicit casting (narrowing)
+
+//                 double num3 = 3.1;
+//                 float num4 = (float) num3;
+//
+//                System.out.println(num4);
+
+	}
+
+}
