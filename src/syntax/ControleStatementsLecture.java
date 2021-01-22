d2e6db671380efb16c4a264ef7a767a79feae228
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-01-22 16:56:37 -0600

    .

--- src/syntax/ControleStatementsLecture.java
+++ src/syntax/ControleStatementsLecture.java
@@ -11,9 +11,9 @@ public class ControleStatementsLecture {
 		}
 
 		char letter = ';';
-		if(letter == 'y') {
+		if (letter == 'y') {
 			System.out.println("The letter was y. I guess that means Yes.");
-		} else if (letter == 'n'){
+		} else if (letter == 'n') {
 			System.out.println("So you are saying no?");
 		} else {
 			System.out.println("I don't know what that letter means.");
@@ -26,7 +26,7 @@ public class ControleStatementsLecture {
 
 
 		int subtotal = 67;
-		System.out.println(subtotal != 0);				// not equal to a numeric literal
+		System.out.println(subtotal != 0);                // not equal to a numeric literal
 
 		String accountOwner = "johnnyf";
 		String loggedInUser = accountOwner;
@@ -36,16 +36,33 @@ public class ControleStatementsLecture {
 		System.out.println(loggedInUser == accountOwner);
 
 		int years = 17;
-		System.out.println(years > 0);					// greater than a numeric literal
+		System.out.println(years > 0);                    // greater than a numeric literal
 		int i = 7;
-		int months =12;
-		System.out.println(i < months);					// less than a variable
+		int months = 12;
+		System.out.println(i < months);                    // less than a variable
 
-		System.out.println(subtotal >= 500);			// greater than or equal to a numeric literal
+		System.out.println(subtotal >= 500);            // greater than or equal to a numeric literal
 		int quantity = subtotal;
 		int reorderPoint = 500;
-		if (quantity <= reorderPoint) {					// less than or equal to a variable
+		if (quantity <= reorderPoint) {                    // less than or equal to a variable
 			System.out.println("We need to order more");
+		}
+
+		// print the numbers 1 through 5
+
+		for (int h = 1; h < 100; h++) {
+			System.out.println(h);
+			if (h == 5) {
+				break; // --------------+
+			} //                        |
+		} //                            |
+		// <----------------------------+
+
+		for (int j = 1; j <= 10; j++) { // <--+
+			if (j % 2 == 1) { //              |
+				continue; //  ----------------+
+			}
+			System.out.println(j);
 		}
 	}
 }
