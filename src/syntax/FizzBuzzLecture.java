1982a844c202a4e104c5c91f12391778d3c19aa3
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-01-22 16:50:39 -0600

    FizzBuzz Lecture Finish

---
+++ src/syntax/FizzBuzzLecture.java
@@ -0,0 +1,48 @@
+package syntax;
+
+public class FizzBuzzLecture {
+
+	static void fizzBuzzFor(int num) {
+		for (int i = 1; i<=num; i++) {
+			if (i % 15 == 0) {
+				System.out.println("FizzBuzz");
+			} else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} else	if (i % 5 == 0) {
+				System.out.println("Buzz");
+			} else {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+	static void fizzBuzzWhile(int num) {
+		// initialize i
+		int i = 1;
+		// while i less than num
+		while (i <= num) {
+			// if i div by 15 output FizzBuzz
+			if (i % 15 == 0) {
+				System.out.println("FizzBuzz");
+			} else
+				// if i div by 3 output Fizz
+				if (i % 3 == 0) {
+					System.out.println("Fizz");
+				} else
+					// if i div by 5 output Buzz
+					if (i % 5 == 0) {
+						System.out.println("Buzz");
+					} else {
+						System.out.println(i);
+					}
+			// increment i
+			i++;
+		}
+	}
+
+	public static void main(String[] args) {
+		int n = 30;
+		fizzBuzzFor(n);
+	}
+}
