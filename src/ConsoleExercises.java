1f8c232249a38d621f3cd6b57c92825c3033406a
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-01-22 15:01:11 -0600

    Java ConsoleExercises Corrected

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -24,55 +24,55 @@ public class ConsoleExercises {
 		//  "The value of pi is approximately 3.14."
 		//  Don't change the value of the variable;
 		//  instead, reference one of the links above and use System.out.format to accomplish this.
-		double pi = 3.14159;
+		double pi = 3.141592;
 
-//		System.out.format("The value of pi is approximately %.2f%n", pi);
+		System.out.format("The value of pi is approximately %.2f%n", pi);
 
 
 		// TODO: Prompt a user to enter a integer and store that value in an int variable using the nextInt method.
 
-//		Scanner scanner = new Scanner(System.in);
-//		System.out.println("Please enter an interger: ");
-//		int userInt = scanner.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter an interger:");
+		int userInt = scanner.nextInt();
 
-//		System.out.println(userInt);
+		System.out.println(userInt);
 
 
 		// TODO: Prompt a user to enter 3 words, and store each of them in a separate variable.
 		//  Then, display them back in the console, each on a newline.
 
-//		System.out.println("Please enter 3 words: ");
-//		String userInput1 = scanner.next();
-//		String userInput2 = scanner.next();
-//		String userInput3 = scanner.next();
+		System.out.println("Please enter 3 words:");
+		String userInput1 = scanner.next();
+		String userInput2 = scanner.next();
+		String userInput3 = scanner.next();
 
-//		System.out.println(userInput1 + "\n" + userInput2 + "\n" + userInput3);
-
+		System.out.println(userInput1 + "\n" + userInput2 + "\n" + userInput3);
+		scanner.nextLine(); // clears the input
 
 		// TODO: Prompt a user to enter a sentence, then store that sentence in a String variable using the next method.
 		//  Then, display that sentence back to the user.
 		// TODO: Rewrite the above example using the nextLine method.
 
-//		System.out.println("Please enter a sentence: ");
+		System.out.println("Please enter a sentence:");
 //		String userSentence = scanner.next();
-//		String userSentence = scanner.nextLine();
+		String userSentence = scanner.nextLine();
 
-//		System.out.print(userSentence);
+		System.out.print(userSentence);
 
 
 		// TODO: Prompt the user to enter values of length and width of a classroom at Codeup.
 		//  Use the nextLine method to get user input and parse the resulting string to a numeric type.
 		// TODO: Display the area and perimeter of that classroom.
 
-//		System.out.println("Please enter the length of the room: ");
-//		String length = scanner.nextLine();
-//		System.out.println("Please enter the width of the room: ");
-//		String width = scanner.nextLine();
+		System.out.println("Please enter the length of the room:");
+		String length = scanner.nextLine();
+		System.out.println("Please enter the width of the room:");
+		String width = scanner.nextLine();
 
-//		int area = Integer.parseInt(length) * Integer.parseInt(width);
-//		int perimeter = 2 * (Integer.parseInt(length) + Integer.parseInt(width));
+		int area = Integer.parseInt(length) * Integer.parseInt(width);
+		int perimeter = 2 * (Integer.parseInt(length) + Integer.parseInt(width));
 
-//		System.out.printf("Area: " + area + "\nPerimeter: " + perimeter);
+		System.out.printf("Area: " + area + "\nPerimeter: " + perimeter);
 
 	}
 
