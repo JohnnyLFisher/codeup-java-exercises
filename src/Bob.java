9ea02d8feaad2abe94138323f14ce443fbc8f122
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-01-25 14:52:21 -0600

    Bob Exercise Start

---
+++ src/Bob.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Bob {
+
+	public static void main(String[] args) {
+
+		/** A Conversation With Bob */
+
+		// TODO: Write the Java code necessary so that a user of your command line application can have a conversation with Bob.
+		//  Bob answers 'Sure.' if you ask him a question. (the input ends with a question mark)
+		//  He answers 'Whoa, chill out!' if you yell at him. (the input ends with an exclamation mark)
+		//  He says 'Fine. Be that way!' if you address him without actually saying anything. (empty input)
+		//  He answers 'Whatever.' to anything else.
+
+	}
+
+}
