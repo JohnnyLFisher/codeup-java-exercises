92092191a333c0d3d31b0ee5879cc7a807aa170d
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-01-25 15:02:27 -0600

    String Exercise Start

---
+++ src/StringExercise.java
@@ -0,0 +1,25 @@
+public class StringExercise {
+
+	public static void main(String[] args) {
+
+		/** String Exercise */
+
+		// TODO:
+		//  For each of the following output examples
+		//  create a String variable that contains the desired output and print it out to the console.
+		//  You can do this with only one String variable and one print statement for each output example.
+		/* --------
+			We don't need no education
+			We don't need no thought control
+			--------
+			Check "this" out!, "s inside of "s!
+			--------
+			In windows, the main drive is usually C:\
+			--------
+			I can do backslashes \, double backslashes \\,
+			and the amazing triple backslash \\\!
+			-------- */
+
+	}
+
+}
