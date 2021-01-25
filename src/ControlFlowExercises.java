469f585510817a40b8cbcd9b090fcf4066c3078b
JohnnyLFisher <johnny.luel.fisher@gmail.com>
2021-01-25 14:23:38 -0600

    Control Flow Exercises Start

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class ControlFlowExercises {
+
+	public static void main(String[] args) {
+
+		/** Control Flow Exercises */
+
+		// TODO: Create an integer variable i with a value of 5.
+
+		// TODO: Create a while loop that runs so long as i is less than or equal to 15.
+
+
+		// TODO: Each loop iteration, output the current value of i, then increment i by one.
+		//  Your output should look like this: 5 6 7 8 9 10 11 12 13 14 15
+
+
+		// TODO: Create a do-while loop that will count by 2's starting with 0 and ending at 100.
+		//  Follow each number with a new line.
+
+
+		// TODO: Alter your loop to count backwards by 5's from 100 to -10.
+
+
+		// TODO: Create a do-while loop that starts at 2.
+		//  displays the number squared on each line while the number is less than 1,000,000.
+		/*  Output should equal:
+			2
+			4
+			16
+			256
+			65536
+		*/
+
+
+		// TODO: Refactor the previous two exercises to use a for loop instead.
+
+
+		// TODO: Recreate FizzBuzz
+
+
+		// TODO: Prompt the user to enter an integer.
+
+
+		// TODO: Display a table of squares and cubes from 1 to the value entered.
+
+
+		// TODO: Ask if the user wants to continue.
+
+
+		// TODO: Assume that the user will enter valid data.
+
+
+		// TODO: Only continue if the user agrees to.
+		/*  Example Output:
+			What number would you like to go up to? 5
+
+			Here is your table!
+
+			number | squared | cubed
+			------ | ------- | -----
+			1      | 1       | 1
+			2      | 4       | 8
+			3      | 9       | 27
+			4      | 16      | 64
+			5      | 25      | 125
+		*/
+
+
+		// TODO: Prompt the user for a numerical grade from 0 to 100.
+
+
+		// TODO: Display the corresponding letter grade.
+
+
+		// TODO: Prompt the user to continue.
+
+
+		// TODO: Assume that the user will enter valid integers for the grades.
+
+
+		// TODO: The application should only continue if the user agrees to.
+		/*  Grade Ranges:
+			A : 100 - 88
+			B : 87 - 80
+			C : 79 - 67
+			D : 66 - 60
+			F : 59 - 0
+		*/
+	}
+}
